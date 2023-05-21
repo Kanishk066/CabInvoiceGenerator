a715e98943c1a5d5a259c9db00ed7792eb889c3c
Kanishk <kanishksinghal06@gmail.com>
2023-05-21 13:55:01 +0530

    Added the code for multiple rides for invoice generator

--- src/main/java/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -1,5 +1,8 @@
 package cabinvoicegenerator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CabInvoiceGenerator {
      static final double COST_PER_KM = 10.0;
      static final double COST_PER_MINUTE = 1.0;
@@ -9,14 +12,38 @@ public class CabInvoiceGenerator {
         double totalFare = distance * COST_PER_KM + minutes * COST_PER_MINUTE;
         return Math.max(totalFare, MINIMUM_FARE);
     }
+    public double calculateTotalFare(List<Ride> rides){
+        double totalFare = 0.0;
+        for (Ride ride : rides) {
+            totalFare += calculateFare(ride.getDistance(), ride.getMinutes());
+        }
+        return totalFare;
+    }
     public static void main(String[] args) {
-        double distance = 10.5;  // Distance in kilometers
-        int minutes = 20;  // Time in minutes
+        List<Ride> rides = new ArrayList<>();
+        rides.add(new Ride(10.5, 20));  // Ride 1
+        rides.add(new Ride(5.0, 15));   // Ride 2
 
         CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
-        double fare = invoiceGenerator.calculateFare(distance, minutes);
+        double totalFare = invoiceGenerator.calculateTotalFare(rides);
 
-        System.out.println("Total Fare: $" + fare);
+        System.out.println("Aggregate Total Fare: $" + totalFare);
+    }
+}
+class Ride {
+    private double distance;
+    private int minutes;
 
+    public Ride(double distance, int minutes) {
+        this.distance = distance;
+        this.minutes = minutes;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public int getMinutes() {
+        return minutes;
     }
 }
